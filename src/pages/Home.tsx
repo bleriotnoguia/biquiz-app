9a6f2deb2fc3ca59123a35566da0fac731aea82a
Bleriot Noguia <nstevebleriot@yahoo.fr>
2022-11-14 17:11:15 +0100

    UI design updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonIcon, IonLabel, IonButton, IonRadio, IonRadioGroup, IonListHeader, IonItemDivider } from '@ionic/react';
 import { bookSharp, play } from 'ionicons/icons';
-import {useState} from 'react'
 import categories from '../data/categories.json'
 import { useDispatch, useSelector } from 'react-redux';
 import { decrement, increment, selectCounter } from '../Reducer';
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 interface CategoryConfig{
   id: number;
@@ -11,15 +12,8 @@ interface CategoryConfig{
 }
 
 const Home: React.FC = () => {
-  const [category, setCategory] = useState<CategoryConfig>(categories[0]);
-
   const counter = useSelector(selectCounter);
   const dispatch = useDispatch();
-
-  const handleCategorySelected = (category_id: number) => {
-    var category_selected = categories.find((item) => item.id === category_id)
-    setCategory(category_selected ?? {id: 1, name: 'hommes'})
-  }
 
   return (
     <IonPage>
@@ -33,22 +27,18 @@ const Home: React.FC = () => {
       </IonHeader>
     
       <IonContent fullscreen>
-        <IonList>
-          <IonRadioGroup value={category.id ?? 1} onIonChange={e => handleCategorySelected(e.detail.value)}>
-            <IonListHeader>
-              <IonLabel>Selectionner une categories</IonLabel>
-            </IonListHeader>
-            {categories.map((category: CategoryConfig, idx) => (<IonItem key={idx}>
-              <IonIcon icon={bookSharp} slot="start" />
-              <IonLabel className="ion-text-capitalize">{category.name}</IonLabel>
-              <IonRadio slot="end" value={category.id} />
-            </IonItem>))}
-          </IonRadioGroup>
-          <IonItemDivider>{'Quiz biblique de categorie' + ' ' + category.name}</IonItemDivider>
-        </IonList>
-        <div className="ion-text-center">
-          <IonButton routerLink={`/page/quiz/category/${category.id}`} color="primary"><IonIcon icon={play} slot="start" /> Commencer</IonButton>
-        </div>
+        <h3 style={{padding: '0px 0.7em'}}>Selectionner une categories</h3>
+        {categories.map((category: CategoryConfig, idx) => (
+          <div className={styles.cardCategory}>
+            <IonIcon icon={bookSharp} slot="start" className={styles.iconCategory} /> 
+            <Link to={`/page/quiz/category/${category.id}`} className={styles.linkStyle} >
+              <div>
+                <h1 style={{fontWeight: 'bold', fontSize: '1.5em'}}>{category.name}</h1>
+                <p>Testez votre connaissance des personnages de la bible</p>
+              </div>
+            </Link>
+          </div>
+        ))}
         <IonItem>
           Count : {counter?.count || 0}
           <IonButton slot="end" onClick={() => dispatch(increment())} > Increment</IonButton>
