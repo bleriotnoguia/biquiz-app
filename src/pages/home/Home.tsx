2e394cf30f0075c78625cf8b2ce6583a7ee8aba1
Bleriot Noguia <nstevebleriot@yahoo.fr>
2023-01-16 20:17:06 +0100

    Refactoring & Display total stars

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,22 +1,36 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonIcon } from '@ionic/react';
-import { bookSharp, starOutline, star, lockClosed } from 'ionicons/icons';
-import styles from './Home.module.css';
-import { Link } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../hooks';
-import { useEffect } from 'react';
-import { fetchCategories } from '../../slices/categoriesSlice';
-import { deleteChoices } from '../../slices/currentQuizSlice';
-import { getStars } from '../../utils';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonIcon,
+  IonButton,
+} from "@ionic/react";
+import {
+  bookSharp,
+  lockClosed,
+  starSharp,
+} from "ionicons/icons";
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { useEffect } from "react";
+import { fetchCategories } from "../../slices/categoriesSlice";
+import { deleteChoices } from "../../slices/currentQuizSlice";
+import { getStars } from "../../utils";
 
 const Home: React.FC = () => {
-  const categories = useAppSelector(state => state.categories)
-  const scores = useAppSelector(state => state.scores.data)
+  const categories = useAppSelector((state) => state.categories);
+  const scores = useAppSelector((state) => state.scores.data);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(fetchCategories())
-  }, [])
-  
+    dispatch(fetchCategories());
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -25,33 +39,56 @@ const Home: React.FC = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle>Accueil</IonTitle>
+          <IonButtons slot="end">
+            <IonButton>
+              <b style={{ marginRight: "3px", fontSize: "1.1em" }}>
+                {scores.map((a) => a.stars).reduce((p, n) => p + n)}
+              </b>
+              <IonIcon icon={starSharp} />
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
-    
+
       <IonContent fullscreen>
-        <h3 style={{padding: '0px 0.7em', textAlign: "center"}}>Selectionner une categories</h3>
+        <h3 style={{ padding: "0px 0.7em", textAlign: "center" }}>
+          Selectionner une categories
+        </h3>
         {categories.data.map((category, idx) => {
-          let category_score = scores.find(score => parseInt(score.category_id) === category?.id)
+          let category_score = scores.find(
+            (score) => parseInt(score.category_id) === category?.id
+          );
           return (
             <div className={styles.cardCategory} key={idx}>
-              <IonIcon icon={bookSharp} slot="start" className={styles.iconCategory} /> 
-              <Link to={`/page/quiz/category/${category?.id}`} onClick={() => dispatch(deleteChoices())} className={styles.linkStyle} >
+              <IonIcon
+                icon={bookSharp}
+                slot="start"
+                className={styles.iconCategory}
+              />
+              <Link
+                to={`/page/quiz/category/${category?.id}`}
+                onClick={() => dispatch(deleteChoices())}
+                className={styles.linkStyle}
+              >
                 <div>
                   <div className={styles.cardCategoryHeader}>
-                    <h1 className={styles.cardCategoryTitle}>{category?.name}</h1>
-                    <IonIcon style={{fontSize: "1.2em"}} icon={lockClosed} />
+                    <h1 className={styles.cardCategoryTitle}>
+                      {category?.name}
+                    </h1>
+                    <IonIcon style={{ fontSize: "1.2em" }} icon={lockClosed} />
                   </div>
                   {/* <p>{category?.description}</p> */}
-                  <p style={{marginTop: "0"}}>Testez votre connaissance des écritures</p>
-                  <div style={{fontSize: "1.5em"}}>
+                  <p style={{ marginTop: "0" }}>
+                    Testez votre connaissance des écritures
+                  </p>
+                  <div style={{ fontSize: "1.5em" }}>
                     {getStars(category_score?.stars ?? 0)}
                   </div>
                 </div>
               </Link>
             </div>
-          )
-        }
-        )}
+          );
+        })}
       </IonContent>
     </IonPage>
   );
