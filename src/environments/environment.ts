f99f8ec740c8b71b02a223e9777b06fa68414d22
Bleriot Noguia <nstevebleriot@yahoo.fr>
2023-10-03 07:50:45 +0100

    supabaseKey & supabaseUrl updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://test.supabase.co',
-  supabaseKey: 'test'
+  supabaseUrl: 'https://ijjhowzxolzsfeucfqui.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqamhvd3p4b2x6c2ZldWNmcXVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU4MTI2NTMsImV4cCI6MTk5MTM4ODY1M30.BpBqRgf5sCJdvsuWDgf4QTNra3z0BSY5WQ7t115456k'
 };
