96ded328efe42b9c771eb51ffee78367aa7646a2
Bleriot Noguia <nstevebleriot@yahoo.fr>
2024-05-22 07:52:16 +0100

    few changes added

--- src/slices/categoriesSlice.ts
+++ src/slices/categoriesSlice.ts
@@ -1,59 +1,63 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { createClient } from '@supabase/supabase-js'
-import { environment } from '../environments/environment';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { createClient } from "@supabase/supabase-js";
+import { environment } from "../environments/environment";
 
-export interface CategoryConfig{
-    id: number;
-    name: string;
-    level: number;
-    is_active: number;
-    parent_id: number | null;
-  }
+export interface CategoryConfig {
+  id: number;
+  name: string;
+  level: number;
+  is_active: number;
+  parent_id: number | null;
+}
 
-interface  CategoriesState {
+interface CategoriesState {
   loading: boolean;
   error: Error | null;
-  data: CategoryConfig[]
+  data: CategoryConfig[];
 }
 
 const defaultCategoriesState: CategoriesState = {
   error: null,
   loading: false,
   data: [],
-}
+};
 
-export const fetchCategories = createAsyncThunk("categories/fetch", async () => {
-  // Initialize the JS client
+export const fetchCategories = createAsyncThunk(
+  "categories/fetch",
+  async () => {
+    // Initialize the JS client
+    const lang = localStorage.language ? localStorage.language : "fr";
 
-  // const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-  // const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-  console.log("supabaseUrl", process.env.REACT_APP_SUPABASE_URL);
+    // const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+    // const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+    // console.log("supabaseUrl", process.env.REACT_APP_SUPABASE_URL);
 
-  const supabaseUrl = environment.supabaseUrl;
-  const supabaseKey = environment.supabaseKey;
+    const supabaseUrl = environment.supabaseUrl;
+    const supabaseKey = environment.supabaseKey;
 
-  const supabase = createClient(supabaseUrl, supabaseKey)
-  let { data: question_categories, error } = await supabase
-  .from('question_categories')
-  .select('id,level,is_active,parent_id, name:question_category_translations(name)')
-  .eq('question_category_translations.locale', 'en')
-  // const lang = localStorage.language ? localStorage.language : 'fr'
-  // const res = await axios.get(`https://biquiz.herokuapp.com/api/${lang}/question-categories`)
-  // return res.data
-  const categories = question_categories?.map(cat => {
-    let [cat_name] = cat.name as any;
-    cat.is_active = cat.is_active ? 1 : 0
-    return {...cat, ...cat_name};
-  });
-  return {data: categories}
+    const supabase = createClient(supabaseUrl, supabaseKey);
+    let { data: question_categories, error } = await supabase
+      .from("question_categories")
+      .select(
+        "id,level,is_active,parent_id, name:question_category_translations(name)"
+      )
+      .eq("question_category_translations.locale", lang);
+    // const res = await axios.get(`https://biquiz.herokuapp.com/api/${lang}/question-categories`)
+    // return res.data
+    const categories = question_categories?.map((cat) => {
+      let [cat_name] = cat.name as any;
+      cat.is_active = cat.is_active ? 1 : 0;
+      return { ...cat, ...cat_name };
+    });
+    return { data: categories };
   }
 );
 
 const categoriesSlice = createSlice({
-	initialState: defaultCategoriesState,
-	name: 'categories',
-	reducers: {},
+  initialState: defaultCategoriesState,
+  name: "categories",
+  reducers: {},
   extraReducers: {
     [fetchCategories.pending.type]: (state) => {
       state.loading = true;
@@ -64,11 +68,10 @@ const categoriesSlice = createSlice({
     },
     [fetchCategories.rejected.type]: (state, action) => {
       console.error(action);
-      state.error = action.error
+      state.error = action.error;
       state.loading = false;
     },
   },
 });
 
-
-export const categoriesReducer = categoriesSlice.reducer
+export const categoriesReducer = categoriesSlice.reducer;
--- src/slices/currentQuizSlice.ts
+++ src/slices/currentQuizSlice.ts
@@ -1,114 +1,118 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { createClient } from '@supabase/supabase-js'
-import { environment } from '../environments/environment';
-import axios from 'axios';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createClient } from "@supabase/supabase-js";
+import { environment } from "../environments/environment";
+import axios from "axios";
 
 export interface QuestionOption {
-    id: number;
-    name: string;
-    is_correct: boolean;
+  id: number;
+  name: string;
+  is_correct: boolean;
 }
 
 export interface Question {
-    id: number;
-    name: string;
-    source_text: string;
-    options: QuestionOption[];
-    question_category_id: number;
+  id: number;
+  name: string;
+  source_text: string;
+  options: QuestionOption[];
+  question_category_id: number;
 }
 
 export interface Choice {
-    question_id: number;
-    choice_id: number;
+  question_id: number;
+  choice_id: number;
 }
 
-interface  CurrentQuizState {
+interface CurrentQuizState {
   questions: Question[];
   choices: Choice[];
   loading: boolean;
   error: Error | null;
 }
 
-const defaultCurrentQuizState:CurrentQuizState = {
-    error: null,
-    loading: false,
-    questions: [],
-    choices: []
-}
+const defaultCurrentQuizState: CurrentQuizState = {
+  error: null,
+  loading: false,
+  questions: [],
+  choices: [],
+};
 
-export const fetchQuestions = createAsyncThunk("questions/fetch", async (arg: {category_id: string}) => {
-      // Initialize the JS client
-      
-      // const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-      // const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+export const fetchQuestions = createAsyncThunk(
+  "questions/fetch",
+  async (arg: { category_id: string }) => {
+    // Initialize the JS client
+    const lang = localStorage.language ? localStorage.language : "fr";
 
-      const supabaseUrl = environment.supabaseUrl;
-      const supabaseKey = environment.supabaseKey;
+    // const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+    // const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-      const supabase = createClient(supabaseUrl, supabaseKey)
-      let { data: data_questions, error } = await supabase
-      .from('questions')
-      .select(`id, 
+    const supabaseUrl = environment.supabaseUrl;
+    const supabaseKey = environment.supabaseKey;
+
+    const supabase = createClient(supabaseUrl, supabaseKey);
+    let { data: data_questions, error } = await supabase
+      .from("questions")
+      .select(
+        `id, 
           is_active,
           source_text:question_translations(source_text, locale), 
           name:question_translations(name, locale), 
-          options:question_options(id, name:question_option_translations(name, locale), is_correct)`)
+          options:question_options(id, name:question_option_translations(name, locale), is_correct)`
+      )
       // .eq('question_translations.locale', 'en')
-      .eq('question_category_id', arg.category_id)
+      .eq("question_category_id", arg.category_id);
 
-      // const lang = localStorage.language ? localStorage.language : 'fr'
-      // const res = await axios.get(`https://biquiz.herokuapp.com/api/${lang}/question-category/${arg.category_id}`)
-      // return res.data
+    // const res = await axios.get(`https://biquiz.herokuapp.com/api/${lang}/question-category/${arg.category_id}`)
+    // return res.data
 
-      const questions = data_questions?.map((ques: any) => {
-        let ques_source_text = ques.source_text as Array<any>;
-        let source_text = ques_source_text.find((item) => item.locale === 'en').source_text;
-        let ques_name = ques.name as Array<any>;
-        let name = ques_name.find((item) => item.locale === 'en').name;
-        ques.is_active = ques.is_active ? 1 : 0;
-        let options = ques.options?.map((opt: any) => {
+    const questions = data_questions?.map((ques: any) => {
+      let ques_source_text = ques.source_text as Array<any>;
+      let source_text = ques_source_text.find(
+        (item) => item.locale === lang
+      ).source_text;
+      let ques_name = ques.name as Array<any>;
+      let name = ques_name.find((item) => item.locale === lang).name;
+      ques.is_active = ques.is_active ? 1 : 0;
+      let options =
+        ques.options?.map((opt: any) => {
           let opt_name = opt.name as Array<any>;
-          let name = opt_name.find((item: any) => item.locale === 'en').name;
-          return {...opt, name};
+          let name = opt_name.find((item: any) => item.locale === lang).name;
+          return { ...opt, name };
         }) ?? [];
-        return {...ques, name, source_text, options};
-      });
-      return {data: questions};
-    }
-  );
+      return { ...ques, name, source_text, options };
+    });
+    return { data: questions };
+  }
+);
 
 const currentQuizSlice = createSlice({
-	initialState: defaultCurrentQuizState,
-	name: 'currentQuiz',
-	reducers: {
-        addChoice: (state, data) => {
-            var newChoices = [data.payload, ...state.choices]
-			      state.choices = newChoices
-        },
-        deleteChoices: (state) => {
-            state.choices = []
-        }
+  initialState: defaultCurrentQuizState,
+  name: "currentQuiz",
+  reducers: {
+    addChoice: (state, data) => {
+      var newChoices = [data.payload, ...state.choices];
+      state.choices = newChoices;
     },
-    extraReducers: {
-      [fetchQuestions.pending.type]: (state) => {
-        state.loading = true;
-      },
-      [fetchQuestions.fulfilled.type]: (state, action) => {
-        // tate.questions = action.payload.data.questions;
-        state.questions = action.payload.data;
-        state.loading = false;
-      },
-      [fetchQuestions.rejected.type]: (state, action) => {
-        console.error(action);
-        state.loading = false;
-      },
+    deleteChoices: (state) => {
+      state.choices = [];
     },
+  },
+  extraReducers: {
+    [fetchQuestions.pending.type]: (state) => {
+      state.loading = true;
+    },
+    [fetchQuestions.fulfilled.type]: (state, action) => {
+      // tate.questions = action.payload.data.questions;
+      state.questions = action.payload.data;
+      state.loading = false;
+    },
+    [fetchQuestions.rejected.type]: (state, action) => {
+      console.error(action);
+      state.loading = false;
+    },
+  },
 });
 
 //actions
-export const {
-    deleteChoices,
-    addChoice
-} = currentQuizSlice.actions;
+export const { deleteChoices, addChoice } = currentQuizSlice.actions;
 
-export const currentQuizReducer = currentQuizSlice.reducer
+export const currentQuizReducer = currentQuizSlice.reducer;
